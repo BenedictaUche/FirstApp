c3c85a49b5edc9f7224d14e684109397ddaff09f
BenedictaUche <benedictaonyebuchi.uche@gmail.com>
2024-10-26 00:45:58 +0100

    update: added new update

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>My first app</Text>
       <StatusBar style="auto" />
+
+      <Pressable
+      onPress={() => Alert.alert('You pressed the text!')}
+      style={styles.button}
+    >
+      <Text>You can press me</Text>
+    </Pressable>
     </View>
+
   );
 }
 
@@ -17,4 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#007bff',
+  }
 });
