fbb4b200962f6241c9940b8b0c57b3daaef75cfa
BenedictaUche <benedictaonyebuchi.uche@gmail.com>
2024-10-27 14:03:55 +0100

    update: added routing

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,24 +1,45 @@
-import { View, StyleSheet, Pressable, Text } from "react-native";
+import { View, StyleSheet, Pressable, Text, ScrollView } from "react-native";
 import Constants from "expo-constants";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    // ...
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#24292e",
+    gap: 5
   },
+
   button: {
-    backgroundColor:  "#24292e",
     padding: 10,
+  },
+
+  scroll: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent:  "center",
   }
-  // ...
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable style={styles.button}>
-        <Text style={{ color: "#fff" }}>Repositories</Text>
-      </Pressable>
+      <ScrollView horizontal>
+        <Pressable style={styles.button}>
+          <Link to="/">
+            <Text style={{ color: "#fff" }}>Repositories</Text>
+          </Link>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Link to="/signIn">
+            <Text style={{ color: "#fff" }}>Sign in</Text>
+          </Link>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,31 @@
-import Constants from 'expo-constants';
-import { StyleSheet, View } from 'react-native';
-import RepositoryList from './RepositoryList';
-import Text from './Text';
-import AppBar from './AppBar';
+import Constants from "expo-constants";
+import { StyleSheet, View } from "react-native";
+import { Route, Routes, Navigate } from "react-router-native";
+import RepositoryList from "./RepositoryList";
+import Text from "./Text";
+import AppBar from "./AppBar";
+import SignIn from "./SignIn";
 
 const styles = StyleSheet.create({
   container: {
     // marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: "#e1e4e8",
   },
 });
 
 const Main = () => {
   return (
     <View style={styles.container}>
-        <AppBar />
-      <Text fontWeight="bold" fontSize="subheading">Rate Repository go lite Application</Text>
-      <RepositoryList />
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/signin" element={<SignIn />}  />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      {/* <Text fontWeight="bold" fontSize="subheading">Rate Repository go lite Application</Text> */}
     </View>
   );
 };
